804f5765d29188af59cd014a65ccd8002e514423
Thirfir <sangbbang31@gmail.com>
2025-01-02 23:12:46 +0900

    gradle 세팅 수정

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,6 +45,10 @@ android {
 
 dependencies {
 
+    implementation(project(":feature:authentication"))
+    implementation(project(":domain"))
+    implementation(project(":data"))
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
